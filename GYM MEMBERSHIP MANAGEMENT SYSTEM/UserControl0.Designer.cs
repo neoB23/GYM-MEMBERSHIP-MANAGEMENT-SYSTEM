7c8d72ad5687797de23f98b12b0854cf689e9f9a
Justin Parlan <justinparlan123@gmail.com>
2024-04-07 02:53:18 +0800

    Receipt in crystal report

--- GYM MEMBERSHIP MANAGEMENT SYSTEM/UserControl0.Designer.cs
+++ GYM MEMBERSHIP MANAGEMENT SYSTEM/UserControl0.Designer.cs
@@ -28,33 +28,18 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.crystalReportViewer1 = new CrystalDecisions.Windows.Forms.CrystalReportViewer();
             this.SuspendLayout();
-            // 
-            // crystalReportViewer1
-            // 
-            this.crystalReportViewer1.ActiveViewIndex = -1;
-            this.crystalReportViewer1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.crystalReportViewer1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.crystalReportViewer1.Dock = System.Windows.Forms.DockStyle.Fill;
-            this.crystalReportViewer1.Location = new System.Drawing.Point(0, 0);
-            this.crystalReportViewer1.Name = "crystalReportViewer1";
-            this.crystalReportViewer1.Size = new System.Drawing.Size(885, 723);
-            this.crystalReportViewer1.TabIndex = 0;
             // 
             // UserControl0
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.crystalReportViewer1);
             this.Name = "UserControl0";
-            this.Size = new System.Drawing.Size(885, 723);
+            this.Size = new System.Drawing.Size(702, 462);
             this.ResumeLayout(false);
 
         }
 
         #endregion
 
-        private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
+        //private CrystalDecisions.Windows.Forms.CrystalReportViewer crystalReportViewer1;
     }
 }
